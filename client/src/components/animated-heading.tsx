5a5e54b4fd6d645796c1a46aced4cb7115519a05
FaiqBinZahidd <118956627+FaiqBinZahidd@users.noreply.github.com>
2025-07-15 09:03:14 +0700

    Add client/src/components/animated-heading.tsx 1752544994102

---
+++ client/src/components/animated-heading.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export function AnimatedHeading() {
+  return (
+    <div className="animated-heading-wrapper">
+      <div className="animated-heading-content">
+        <p className="animated-heading-content__text">Build, Sell,</p>
+        <ul className="animated-heading-content__list">
+          <li className="animated-heading-content__list__item">Succeed.</li>
+          <li className="animated-heading-content__list__item">Innovate.</li>
+          <li className="animated-heading-content__list__item">Create.</li>
+          <li className="animated-heading-content__list__item">Inspire.</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
