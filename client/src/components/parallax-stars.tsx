fe722798e5b79e674a352538eafa6fb52b2bec4b
FaiqBinZahidd <118956627+FaiqBinZahidd@users.noreply.github.com>
2025-07-15 09:02:54 +0700

    Add client/src/components/parallax-stars.tsx 1752544973343

---
+++ client/src/components/parallax-stars.tsx
@@ -0,0 +1,9 @@
+export default function ParallaxStars() {
+  return (
+    <div className="fixed inset-0 pointer-events-none overflow-hidden z-[-1]">
+      <div id="stars" />
+      <div id="stars2" />
+      <div id="stars3" />
+    </div>
+  );
+}
